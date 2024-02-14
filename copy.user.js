f86d82988f5d5946d9d1fe19b3cfce94cb83cb1c
await <285283010@qq.com>
2024-02-14 15:19:00 +0800

    aa

---
+++ copy.user.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: Await
+ * @Date: 2023-12-17 09:38:48
+ * @LastEditors: Await
+ * @LastEditTime: 2023-12-17 09:55:03
+ * @Description: 请填写简介
+ */
+// ==UserScript==
+// @name         ChatGPT、豆包、Poe 宽屏模式
+// @namespace    https://greasyfork.org/
+// @version      1.2.5
+// @description  页面大于1080的屏幕撑开，显示更宽，看的更多，目前适用于Chatgpt、豆包（doubao）ai网站，后续会适配更多网站，欢迎提供网站，谢谢！
+// @author       Await
+// @match        *://www.qcc.com/web/search/*
+// @match        *://www.qcc.com/firm/*
+// @match        *://aiqicha.baidu.com/company_detail_*
+// @match        *:aiqicha.baidu.com/s?q=*
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  function run() {
+    console.log("sdfsdf");
+  }
+
+  window.onload = function () {
+    run();
+  };
+})();
